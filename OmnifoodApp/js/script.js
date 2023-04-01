6a5f52feaa3d2bab62c18015f08e5291c5ba7b1f
mehdisahin <sahinmehdi23@gmail.com>
2023-04-01 14:54:38 +0300

    add smooth scrolling

---
+++ OmnifoodApp/js/script.js
@@ -0,0 +1,22 @@
+console.log("Hello world!");
+
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
